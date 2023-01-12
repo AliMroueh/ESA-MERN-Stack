d9cd00935eb3714fa6eda5324f7bd4e495d2f2f1
souzan <sasiallouch1234@gmail.com>
2023-01-12 20:55:47 +0200

    .......

--- smile/src/components/mainPage/SlideCard.js
+++ smile/src/components/mainPage/SlideCard.js
@@ -1,26 +1,53 @@
-import React from "react"
-import Sdata from "./Sdata"
-import Slider from "./Slider"
+import React from "react";
+import Sdata from "./Sdata";
+import Slider from "./Slider";
 //import Slider from "react-slick"
-//import "slick-carousel/slick/slick.css"
-//import "slick-carousel/slick/slick-theme.css"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const SlideCard = () => {
 
-  /*const settings = {
-    dots: true,
-    //infinite: true,
-    //speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    //autoplaySpeed:2000,
-    
-  }*/
+ 
+ const SlideCard = () => {
+   
+
+    var settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 3,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
   return (
     <>
    
-      <Slider className="slider">
+      <Slider {...settings} className="slider">
         {Sdata.map((value, index) => {
           return (
             <>
@@ -39,7 +66,9 @@ const SlideCard = () => {
         })}
       </Slider>
     </>
-  )
+  );
 }
+
+
 
 export default SlideCard
