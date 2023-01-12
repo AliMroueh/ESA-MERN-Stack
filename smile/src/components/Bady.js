f86f235752fee1b27317a023366b05d9ae2ebb01
Ali <mroueh_ali@outlook.com>
2023-01-12 18:53:29 +0200

    good

---
+++ smile/src/components/Bady.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import Categories from './categories';
+
+ 
+ const Bady = () => {
+   return (
+    <>
+    <section className='home'>
+        <div className='container d-flex'>
+            <Categories/>
+        </div>
+    </section>
+    </>
+   )
+ }
+ 
+ export default Bady
