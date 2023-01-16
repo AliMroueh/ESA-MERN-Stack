be2221bfb97539efecb9a575994fafb95f57ed90
souzan <sasiallouch1234@gmail.com>
2023-01-16 22:39:49 +0200

    add deleteUser and GetUsers

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -138,4 +138,37 @@ expressAsyncHandler(async(req,res) => {
         res.status(401).send({message: "unKnown id"});
     }
 }))
+userRouter.delete('/:id',expressAsyncHandler(async(req,res)=>{
+   
+    const id = req.params.id;
+
+    await User.findByIdAndDelete(id)
+        .then(data => {
+            if(!data){
+                res.status(404).send({ message : `Cannot Delete with id ${id}. Maybe id is wrong`})
+            }else{
+                res.send({
+                    message : "User was deleted successfully!"
+                })
+            }
+        })
+        .catch(err =>{
+            res.status(500).send({
+                message: "Could not delete User with id=" + id
+            });
+        });
+}))
+
+
+
+//get data
+userRouter.get('/',expressAsyncHandler(async( req ,res)=>{
+    try{
+     const users = await User.find();
+     res.status(200).send(users);
+    }
+    catch(err){
+     res.status(500).send(err);
+    };
+ }));
 export default userRouter;
