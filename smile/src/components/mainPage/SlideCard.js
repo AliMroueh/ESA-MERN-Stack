2240aeaa447ab22b7d763f91d0fb6c92daed3418
Ali <mroueh_ali@outlook.com>
2023-02-02 10:54:16 +0200

    x

--- smile/src/components/mainPage/SlideCard.js
+++ smile/src/components/mainPage/SlideCard.js
@@ -1,42 +1,42 @@
-import React from "react"
-import Carousel from 'react-bootstrap/Carousel';
-import './Categories.css'
-function SlideCard() {
-  return (
-    <Carousel>
-      <Carousel.Item>
-        <div className="es">
-        <img
-          className="d-block "
-          src="images/sale4.webp"
-          alt="First slide"
-        /></div>
-         <Carousel.Caption>
-          <h2  className="h2">50% Off For Your First Shopping</h2>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block"
-          src="images/mohammad-metri-E-0ON3VGrBc-unsplash.jpg"
-          alt="Second slide"
-        />
-          <Carousel.Caption>
-          <h2 className="h2">50% Off For Your First Shopping</h2>
-          </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block"
-          src="images/iphone.jpg"
-          alt="Third slide"
-        />
+// import React from "react"
+// import Carousel from 'react-bootstrap/Carousel';
+// import './Categories.css'
+// function SlideCard() {
+//   return (
+//     <Carousel>
+//       <Carousel.Item>
+//         <div className="es">
+//         <img
+//           className="d-block "
+//           src="images/sale4.webp"
+//           alt="First slide"
+//         /></div>
+//          <Carousel.Caption>
+//           <h2  className="h2">50% Off For Your First Shopping</h2>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//         <img
+//           className="d-block"
+//           src="images/mohammad-metri-E-0ON3VGrBc-unsplash.jpg"
+//           alt="Second slide"
+//         />
+//           <Carousel.Caption>
+//           <h2 className="h2">50% Off For Your First Shopping</h2>
+//           </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//         <img
+//           className="d-block"
+//           src="images/iphone.jpg"
+//           alt="Third slide"
+//         />
 
-        <Carousel.Caption>
-          <h2  className="h2">50% Off For Your First Shopping</h2>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
-}
-export default SlideCard
+//         <Carousel.Caption>
+//           <h2  className="h2">50% Off For Your First Shopping</h2>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//     </Carousel>
+//   );
+// }
+// export default SlideCard
--- smile/src/components/new arrival/Tdata.js
+++ smile/src/components/new arrival/Tdata.js
@@ -1,29 +1,29 @@
-const Tdata = [
-    {
-      cover: "images/category-1.webp",
-      para: "headphone",
-      desc: "3k orders this week",
-    },
-    {
-      cover: "images/category-2.webp",
-      para: "watch",
-      desc: "4k orders this week",
-    },
-    {
-      cover: "images/category-3.webp",
-      para: "sunglass",
-      desc: "6k orders this week",
-    },
-    {
-      cover: "images/category-2.webp",
-      para: "watch",
-      desc: "4k orders this week",
-    },
-    {
-      cover: "images/category-3.webp",
-      para: "sunglass",
-      desc: "6k orders this week",
-    },
-  ]
+// const Tdata = [
+//     {
+//       cover: "images/category-1.webp",
+//       para: "headphone",
+//       desc: "3k orders this week",
+//     },
+//     {
+//       cover: "images/category-2.webp",
+//       para: "watch",
+//       desc: "4k orders this week",
+//     },
+//     {
+//       cover: "images/category-3.webp",
+//       para: "sunglass",
+//       desc: "6k orders this week",
+//     },
+//     {
+//       cover: "images/category-2.webp",
+//       para: "watch",
+//       desc: "4k orders this week",
+//     },
+//     {
+//       cover: "images/category-3.webp",
+//       para: "sunglass",
+//       desc: "6k orders this week",
+//     },
+//   ]
   
-  export default Tdata
+//   export default Tdata
