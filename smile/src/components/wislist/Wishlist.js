ec4afaf7952eca364c81e368f47b86725afd64ff
souzan <sasiallouch1234@gmail.com>
2023-02-05 11:10:15 +0200

    ...

--- smile/src/components/wislist/Wishlist.js
+++ smile/src/components/wislist/Wishlist.js
@@ -1,43 +1,77 @@
-/*import React from 'react'
-import './Wishlist.css'
-const Wishlist = () => {
-  return (
-    <div className='cart'>
-    <h1>Wishlist Cart</h1>
-    <div className='row color-1'>
-        <div>
-            <img src="./images/p1.jpg" alt="hello" className="small"></img>
-        </div>
-        <div>
-            <h3>Pants</h3>
-            <p>Color: </p><p>Red</p>
-        </div>
-        <div>
-            <h3>$251.00</h3>
-        </div>
-        <div>
-        <select value="20">
-                <option value="1">
-                    1
-                </option>
-                <option value="2">
-                    2
-                </option>
-                <option value="3">
-                    3
-                </option>
-                <option value="4">
-                    4
-                </option>
-        </select>
-        </div>
-        <div>
-            <h3>$215.00</h3>
-            <p><i className="fa-solid fa-trash"></i></p>
-        </div>
+/* eslint-disable react-hooks/rules-of-hooks */
+import React from 'react'
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { addToWishlist ,wishlistDeleteAction} from '../../actions/productActions';
+import MessageBox from '../components/MessageBox';
+
+const wishlist = (props) => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const wishlist = useSelector(state => state.wishlist);
+    const { WishlistItems, error } = wishlist;
+
+    const params = useParams();
+    const {id:productId} = params;
+
+    useEffect( () =>{
+        if(productId){
+        dispatch(addToWishlist(productId,quantity))
+        }
+    }, [dispatch, productId, quantity]);
+
+    const deleteWislistHandeler = (id) => {
+        // delete action
+        dispatch(wishlistDeleteAction(id))
+    }
+    const checkoutHandler = () => {
+        navigate('/signin?redirect=/shipping');
+    }
+return (
+    <div className="row top">
+    <div className='col-2'>
+        <h1>Your Wishlist Items</h1>
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
+        {wishlistItems.length === 0?
+        <MessageBox>Wishlist is empty. <Link to="/">Go Shopping</Link></MessageBox>
+    :
+    (
+        <ul>
+            {wishlistItems.map(item => (
+                <li key={item.product}>
+                    <div className="row">
+                        <div>
+                            <img src={item.image} alt='' className="small"></img>
+                        </div>
+                        <div className='min-30'>
+                            <Link to={`/wishlist/${item.product}`}>{item.name}</Link>
+                        </div>
+                        <div>
+                            <select value={item.qty} onChange={
+                                e => dispatch(addToWishlist(item.product,Number(e.target.value)))
+                            }>
+                                {[...Array(item.countInStock).keys()].map(x =>
+                                    <option key={x+1} value={x+1}>
+                                        {x+1}
+                                    </option>
+                                    )}
+                            </select>
+                        </div>
+                        <div>${item.price}</div>
+                        <div>
+                            <button type="button" onClick={() => deleteWislistHandeler(item.product)}>Delete</button>
+                        </div>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    )
+    }
     </div>
-    </div>
-  )
+</div>
+)
 }
 
-export default Wishlist*/
+
+export default wishlist
