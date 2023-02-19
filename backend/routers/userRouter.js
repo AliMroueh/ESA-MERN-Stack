b87bc1d5f9cabe31a8e8141a2371396dade33586
Ali <mroueh_ali@outlook.com>
2023-02-19 22:28:53 +0200

    register validation

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -91,13 +91,13 @@ expressAsyncHandler(async(req,res) => {
 // );
 
 userRouter.post('/register', expressAsyncHandler(async(req,res) => {
-    // const passValidation =  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
-    // const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    // if(!req.body.email.match(emailValidation)){
-    //     res.status(401).send({message : "Email should be like example@gmail.com"});
-    // }else if(!req.body.password.match(passValidation)){
-    //     res.status(401).send({message : "Password should be contain one: upper case, lower case, new number, no white space, minimum 8 chars"})
-    // }else{
+    const passValidation =  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+    const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(!req.body.email.match(emailValidation)){
+        res.status(401).send({message : "Email should be like example@gmail.com"});
+    }else if(!req.body.password.match(passValidation)){
+        res.status(401).send({message : "Password should be contain one: upper case, lower case, new number, no white space, minimum 8 chars"})
+    }else{
     const user = new User({name: req.body.name, email: req.body.email, password: bcrypt.hashSync(req.body.password, 8),
     });
     const createdUser = await user.save();
@@ -111,7 +111,7 @@ userRouter.post('/register', expressAsyncHandler(async(req,res) => {
                 token : generateToken(createdUser),
                 rToken : refresh
             });
-        // }
+        }
     })
     )
 
